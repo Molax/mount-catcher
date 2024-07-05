61d0fdffddc0f18d37d2a97694f89b4798ed2c34
João Paulo Amorim <joaopaulo18@live.it>
2024-07-05 19:01:26 -0300

    feat

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ const arrow = document.getElementById('arrow');
 const yellowArea = document.getElementById('yellowArea');
 const dots = Array.from(document.querySelectorAll('.dot'));
 const timerElement = document.getElementById('timer');
-const timerBackground = document.querySelector('.timer-background');
+const timerText = document.getElementById('timerText');
+const victoryMessage = document.getElementById('victoryMessage');
+const failureMessage = document.getElementById('failureMessage');
 
 // Variáveis globais para a animação da seta
 let arrowPosition = 0;
 let arrowDirection = 1;
 let speedFactor = 1;
 let isPaused = false;
-let isMoving = false; // Adicionado para controle de movimento da seta
+let isMoving = false;
 let timerId;
 let timeLeft = 10;
+let timerPerMonster = 10;
 
-// Funções de manipulação da barra amarela
 function diminuiTamanhoDaBarra() {
     const div = document.getElementById('yellowArea');
     let larguraAtual = div.offsetWidth;
@@ -29,7 +31,6 @@ function resetaTamanhoDaBarra() {
     document.getElementById('yellowArea').style.width = "10px";
 }
 
-// Função principal para mover a seta
 function moveArrow() {
     const inputRect = input.getBoundingClientRect();
     const maxWidth = inputRect.width - arrow.offsetWidth;
@@ -38,26 +39,21 @@ function moveArrow() {
         if (!isPaused && isMoving) {
             arrowPosition += arrowDirection * speedFactor;
 
-            // Verifica se a seta atingiu as bordas
             if (arrowPosition >= maxWidth) {
-                arrowDirection = -1;  // Muda a direção para mover para a esquerda
+                arrowDirection = -1;
             } else if (arrowPosition <= 0) {
-                arrowDirection = 1;  // Muda a direção para mover para a direita
+                arrowDirection = 1;
             }
 
-            // Atualiza a posição da seta
             arrow.style.left = arrowPosition + 'px';
         }
 
-        // Solicita o próximo quadro de animação
         animationFrameId = requestAnimationFrame(frame);
     }
 
-    // Inicia o loop de animação
     requestAnimationFrame(frame);
 }
 
-// Função para aumentar a velocidade da seta
 function increaseSpeed(factor) {
     speedFactor *= factor;
 }
@@ -66,7 +62,6 @@ function resetSpeed() {
     speedFactor = 1;
 }
 
-// Função para pausar a animação por um segundo
 function pauseForOneSecond() {
     isPaused = true;
     setTimeout(() => {
@@ -76,89 +71,71 @@ function pauseForOneSecond() {
 
 function isAligned(arrowRect, yellowRect) {
     const arrowTipCenterX = arrowRect.left + arrowRect.width / 2;
-    const margin = 1; // Define a margem de 1 pixel
+    const margin = 1;
 
-    // Verifica se o centro da ponta da seta está dentro da margem de erro
-    if (arrowTipCenterX >= yellowRect.left - margin && arrowTipCenterX <= yellowRect.right + margin) {
-        return true;
-    } else {
-        return false;
-    }
+    return arrowTipCenterX >= yellowRect.left - margin && arrowTipCenterX <= yellowRect.right + margin;
 }
 
-// Função para mover a área amarela para uma posição aleatória com margem nos extremos
 function moveYellowArea() {
     const inputRect = input.getBoundingClientRect();
     const maxWidth = inputRect.width - yellowArea.offsetWidth;
     const margin = 17;
 
     const newX = Math.random() * (maxWidth - 2 * margin) + margin;
-
     yellowArea.style.left = newX + 'px';
 
     diminuiTamanhoDaBarra();
 }
 
-// Função para lidar com o clique no botão de iniciar
 function handleStartButtonClick() {
     if (!isMoving) {
         isMoving = true;
-        moveArrow(); // Reinicia a animação
+        moveArrow();
 
-        // Reinicia o temporizador
-        timeLeft = 10;
+        timeLeft = timerPerMonster;
         timerElement.textContent = timeLeft;
-        timerElement.style.width = '100%'; // Resetando a largura do temporizador para 100%
-        updateTimerText(); // Atualiza o texto do temporizador
+        timerElement.style.width = '100%';
+        updateTimerText();
         clearInterval(timerId);
         timerId = setInterval(() => {
             timeLeft--;
             timerElement.textContent = timeLeft;
 
-            // Ajusta dinamicamente a largura do temporizador conforme o tempo restante
-            timerElement.style.width = `calc(${(timeLeft / 10) * 100}% - 8px)`; // Ajuste na largura
+            timerElement.style.width = `calc(${(timeLeft / 10) * 100}% - 8px)`;
 
             if (timeLeft <= 0) {
                 clearInterval(timerId);
-                handleResetButtonClick();
+                handleFailure();
+            } else {
+                updateTimerText();
             }
-
-            updateTimerText(); // Atualiza o texto do temporizador
         }, 1000);
     }
 }
 
-// Função para lidar com o evento de resetar
 function handleResetButtonClick() {
-    // Esvazia as bolinhas
-    dots.forEach(dot => dot.classList.remove('active'));
+    Array.from(document.querySelectorAll('.dot')).forEach(dot => dot.classList.remove('active'));
 
-    // Reseta a posição da seta e para a animação
     isMoving = false;
     cancelAnimationFrame(animationFrameId);
     arrowPosition = 0;
     arrowDirection = 1;
     arrow.style.left = arrowPosition + 'px';
 
-    // Reseta a barra amarela
     resetaTamanhoDaBarra();
     moveYellowArea();
 
-    // Reseta a velocidade
     resetSpeed();
 
-    // Reseta o temporizador
     clearInterval(timerId);
-    timeLeft = 10;
+    timeLeft = timerPerMonster;
     timerElement.textContent = timeLeft;
-    timerElement.style.width = '100%'; // Resetando a largura do temporizador para 100% instantaneamente
-    updateTimerText(); // Atualiza o texto do temporizador
+    timerElement.style.width = '100%';
+    updateTimerText();
 }
 
-
-// Função para lidar com o evento de pressionar tecla
 function handleKeyDown(event) {
-    if (event.key === ' ') { // Verifica se a tecla pressionada é o espaço
+    if (event.key === ' ') {
         const arrowRect = arrow.getBoundingClientRect();
         const yellowRect = yellowArea.getBoundingClientRect();
 
@@ -168,31 +145,34 @@ function handleKeyDown(event) {
             arrowPosition = 0;
             arrowDirection = 1;
 
-            // Encontra a próxima bolinha não ativa da direita para a esquerda
-            const nextDot = dots.slice().reverse().find(dot => !dot.classList.contains('active'));
+            let updatedDots = Array.from(document.querySelectorAll('.dot'));
+
+            const nextDot = updatedDots.slice().reverse().find(dot => !dot.classList.contains('active'));
             if (nextDot) {
                 nextDot.classList.add('active');
             }
 
-            // Reinicia o temporizador imediatamente
-            timeLeft = 10;
+            if (updatedDots.every(dot => dot.classList.contains('active'))) {
+                handleVictory();
+            }
+
+            timeLeft = timerPerMonster;
             timerElement.textContent = timeLeft;
-            timerElement.style.width = '100%'; // Resetando a largura do temporizador para 100%
-            updateTimerText(); // Atualiza o texto do temporizador
-            clearInterval(timerId); // Para o temporizador atual, se estiver em execução
+            timerElement.style.width = '100%';
+            updateTimerText();
+            clearInterval(timerId);
             timerId = setInterval(() => {
                 timeLeft--;
                 timerElement.textContent = timeLeft;
 
-                // Ajusta dinamicamente a largura do temporizador conforme o tempo restante
-                timerElement.style.width = `calc(${(timeLeft / 10) * 100}% - 8px)`; // Ajuste na largura
+                timerElement.style.width = `calc(${(timeLeft / timerPerMonster) * 100}% - 8px)`;
 
                 if (timeLeft <= 0) {
                     clearInterval(timerId);
-                    handleResetButtonClick();
+                    handleFailure();
+                } else {
+                    updateTimerText();
                 }
-
-                updateTimerText(); // Atualiza o texto do temporizador
             }, 1000);
         } else {
             pauseForOneSecond();
@@ -202,15 +182,12 @@ function handleKeyDown(event) {
     }
 }
 
-// Função para atualizar o texto do temporizador
 function updateTimerText() {
-    const timerText = document.getElementById('timerText');
     timerText.textContent = `${timeLeft}s`;
 }
 
-// Função para lidar com o evento de toque na tela
 function handleTouchStart(event) {
-    event.preventDefault(); // Evita o comportamento padrão do toque (como zoom)
+    event.preventDefault();
 
     const arrowRect = arrow.getBoundingClientRect();
     const yellowRect = yellowArea.getBoundingClientRect();
@@ -220,6 +197,36 @@ function handleTouchStart(event) {
         moveYellowArea();
         arrowPosition = 0;
         arrowDirection = 1;
+
+        let updatedDots = Array.from(document.querySelectorAll('.dot'));
+
+        const nextDot = updatedDots.slice().reverse().find(dot => !dot.classList.contains('active'));
+        if (nextDot) {
+            nextDot.classList.add('active');
+        }
+
+        if (updatedDots.every(dot => dot.classList.contains('active'))) {
+            handleVictory();
+        }
+
+        timeLeft = timerPerMonster;
+        timerElement.textContent = timeLeft;
+        timerElement.style.width = '100%';
+        updateTimerText();
+        clearInterval(timerId);
+        timerId = setInterval(() => {
+            timeLeft--;
+            timerElement.textContent = timeLeft;
+
+            timerElement.style.width = `calc(${(timeLeft / timerPerMonster) * 100}% - 8px)`;
+
+            if (timeLeft <= 0) {
+                clearInterval(timerId);
+                handleFailure();
+            } else {
+                updateTimerText();
+            }
+        }, 1000);
     } else {
         pauseForOneSecond();
         arrowPosition = 0;
@@ -227,19 +234,97 @@ function handleTouchStart(event) {
     }
 }
 
-// Adiciona listeners de eventos
+function handleVictory() {
+
+    setTimeout(() => {
+        handleResetButtonClick(); // Resetar o jogo completamente
+    }, 500);
+
+
+    // Exibir mensagem de vitória
+    victoryMessage.style.display = 'block';
+
+    // Esconder mensagem de vitória após 2 segundos
+    setTimeout(() => {
+        victoryMessage.style.display = 'none';
+    }, 3500);
+}
+
+function handleFailure() {
+    setTimeout(() => {
+        handleResetButtonClick(); // Resetar o jogo completamente
+    }, 500);
+
+    // Exibir mensagem de falha
+    failureMessage.style.display = 'block';
+
+    // Esconder mensagem de falha após 2 segundos
+    setTimeout(() => {
+        failureMessage.style.display = 'none';
+    }, 3500);
+}
+
+// Função para atualizar as configurações do jogo com base na imagem selecionada
+function updateGameSettings(newTime, newDotsCount,imagePath) {
+
+    const buttonImage = document.querySelector('.button-image');
+    buttonImage.src = imagePath; // Atualiza a imagem de fundo do botão
+    // Atualiza o tempo do temporizador
+    timerPerMonster = newTime;
+    timeLeft = newTime;
+    timerElement.textContent = timeLeft;
+    timerElement.style.width = '100%'; // Resetando a largura do temporizador para 100%
+    updateTimerText(); // Atualiza o texto do temporizador
+
+    // Atualiza a quantidade de bolinhas
+    resetDots(newDotsCount);
+
+    handleResetButtonClick();
+}
+
+// Função para resetar a quantidade de bolinhas com base na imagem selecionada
+function resetDots(count) {
+    const dotsContainer = document.querySelector('.dots');
+    
+    // Remove todas as bolinhas existentes
+    while (dotsContainer.firstChild) {
+        dotsContainer.removeChild(dotsContainer.firstChild);
+    }
+
+    // Adiciona a quantidade correta de bolinhas
+    for (let i = 1; i <= count; i++) {
+        const dot = document.createElement('div');
+        dot.classList.add('dot');
+        dot.id = `dot${i}`;
+        dotsContainer.appendChild(dot);
+    }
+}
+
 function addEventListeners() {
     const startButton = document.getElementById('btnstart');
     const resetButton = document.getElementById('btnreset');
+    const imageOptions = Array.from(document.querySelectorAll('.image-option'));
 
     startButton.addEventListener('click', handleStartButtonClick);
     resetButton.addEventListener('click', handleResetButtonClick);
 
     document.addEventListener('keydown', handleKeyDown);
     document.addEventListener('touchstart', handleTouchStart);
+
+    // Event listener para seleção de imagens
+    imageOptions.forEach(option => {
+        option.addEventListener('click', () => {
+            const time = parseInt(option.getAttribute('data-time'), 10);
+            const dotsCount = parseInt(option.getAttribute('data-dots'), 10);
+            const imagePath = option.getAttribute('data-img');
+            // Atualiza o temporizador e a quantidade de bolinhas
+            updateGameSettings(time, dotsCount ,imagePath);
+        });
+    });
+
+
 }
 
-// Inicia a animação da seta e adiciona os listeners de eventos
 function initialize() {
     addEventListeners();
 
@@ -247,5 +332,4 @@ function initialize() {
     document.getElementById('currentYear').textContent = currentYear;
 }
 
-// Chama a função de inicialização ao carregar a página
 initialize();
